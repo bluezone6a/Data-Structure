d9a91926b0de81cdcb8b7834dec7b3ab061900ba
bluezone6a <79468153+bluezone6a@users.noreply.github.com>
2021-02-23 20:33:24 +0900

    Update 1-2.c

--- submit/CH01/1-2.c
+++ submit/CH01/1-2.c
@@ -8,7 +8,7 @@ int main(void)
 
 	int ary[ROW_SIZE][COL_SIZE] = { {4,2,3},{5,2,3} };
 	int(*p)[COL_SIZE] = ary;
-	int i, j; //indexing 하기위해 i와 j를 쓴다. i다음에 j이고 3차원 배열까지 가게되면 k로 처리한다.
+	int i, j; 
 
 	printf("ary를 이용한 출력\n");
 	for (i = 0; i < ROW_SIZE; i++) {
@@ -16,7 +16,7 @@ int main(void)
 			printf("%d ", ary[i][j]);
 		putchar('\n');
 	}
-	puts("\n"); //같은 파트는 붙혀쓴다.
+	puts("\n"); 
 
 	printf("p를 이용한 출력\n");
 	for (i = 0; i < ROW_SIZE; i++) {
